59fac4e1e4c9daa3ebacb6074c7fb94d11c4a8a1
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-31 21:36:51 +0200

    first test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
+import { isTSAnyKeyword, exportAllDeclaration } from "@babel/types";
+
+it("shows a comment box", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("Box");
+
+  ReactDOM.unmountComponentAtNode(div);
+});
